91df39d03f78e0f5b1baf792b829111b7fe77ad1
Eric Lindow <elindow@verizon.net>
2018-08-31 11:18:10 -0400

    new images and misc

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,9 @@ var myImage = document.querySelector('img');
 myImage.onclick = function() {
 	var mySrc = myImage.getAttribute('src');
 	if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
+      myImage.setAttribute ('src','images/bad error.png');
 	} else {
-	  myImage.setAttribute ('src','images/firefox-icon.png');
+	  myImage.setAttribute ('src','images/Exploding_Computer.jpg');
 	}
 }
 
