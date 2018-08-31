e425743fc0a89d375c0ff413214c6098537b8586
Eric Lindow <elindow@verizon.net>
2018-08-31 11:36:40 -0400

    minor js fixes etc.

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
 	var mySrc = myImage.getAttribute('src');
-	if(mySrc === 'images/firefox-icon.png') {
+	if(mySrc === 'images/Trevor Logo.png') {
       myImage.setAttribute ('src','images/bad error.png');
 	} else {
 	  myImage.setAttribute ('src','images/Exploding_Computer.jpg');
@@ -19,14 +19,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  myHeading.innerHTML = 'Hey' + myName + ', you are cool!';
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'Coding is cool, ' + storedName;
 }
 
 myButton.onclick = function() {
