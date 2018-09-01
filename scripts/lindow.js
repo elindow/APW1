4c7588bba80b26db5d7537a556ffd1dad1cbaf1a
ELindow <elindow@verizon.net>
2018-09-01 11:51:55 -0400

    added js button code

--- scripts/lindow.js
+++ scripts/lindow.js
@@ -1,6 +1,9 @@
+    var LindowButton = document.querySelector('button');
+
+	LindowButton.onclick = function() {  
+	       alert("Lindow button clicked");                                 // mouse click calls 'setUserName()' function
+	}
 
 
 
-
-
-	alert("Lindow here");
+	
