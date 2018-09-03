8a07d9dda0a0c4a7592106cb4de5404385dbff04
ELindow <elindow@verizon.net>
2018-09-02 20:17:38 -0400

    jQuery button code

--- scripts/lindow.js
+++ scripts/lindow.js
@@ -1,12 +1,23 @@
-    var LindowButton = document.querySelector('button');
-
-	LindowButton.onclick = function() {  
-	       alert("Lindow button clicked");                                 // mouse click calls 'setUserName()' function
-	}
-
-$(document).ready(function() {  										   // jQuery will wait until DOM is ready
-    // all custom jQuery will go here
-    $("#jQdemo").html("JQuery is working");
-});
+ 
 
 	
+
+$(document).ready(function() {  										   // jQuery will wait until DOM is ready
+
+    // all custom jQuery will go here
+
+    $("#jQdemo").html("JQuery is working");								   // adds the text to the html of the element, $ at front of statement indicates jQuery, 
+    																	   // # at front of object indicates id, function to be called comes after . 
+    $("LButton1").onclick(alert('Button clicked'));						   // note how much cleaner the code is compared to below
+
+    //var LindowButton = document.querySelector('button'); 					//this code selects the first (and in this case only) button on the page
+	//LindowButton.onclick = function() {  
+	//       alert("Lindow button clicked");                                 // mouse click calls 'setUserName()' function
+	//}
+
+
+
+	// end of jQuery block
+});
+
+		
