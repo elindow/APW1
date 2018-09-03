af8087fb6936da1f3b3ec6d50d4dad9be65a1e05
ELindow <elindow@verizon.net>
2018-09-02 20:40:03 -0400

    jQ button code test

--- scripts/lindow.js
+++ scripts/lindow.js
@@ -8,7 +8,7 @@ $(document).ready(function() {  										   // jQuery will wait until DOM is re
 
     $("#jQdemo").html("JQuery is working");								   // adds the text to the html of the element, $ at front of statement indicates jQuery, 
     																	   // # at front of object indicates id, function to be called comes after . 
-    $("LButton1").onclick(alert('Button clicked'));						   // note how much cleaner the code is compared to below
+    $("LButton1").onclick(function() {alert('Button clicked')});						   // note how much cleaner the code is compared to below
 
     //var LindowButton = document.querySelector('button'); 					//this code selects the first (and in this case only) button on the page
 	//LindowButton.onclick = function() {  
