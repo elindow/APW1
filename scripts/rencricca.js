ad6c16843d04c088ec774af448bb9d85d2a6c7c5
dtrTrevor <drencricca@trevor.org>
2018-10-02 09:08:51 -0400

    new changes

--- scripts/rencricca.js
+++ scripts/rencricca.js
@@ -6,3 +6,18 @@ function checkemail() {
     return false;
   }
 }
+
+
+function showImage() {
+  var pic_element = document.getElementById("new_img");
+  if (pic_element == null) {
+ 	  var img=document.createElement("img");
+    img.id = 'new_img'
+    img.src='../images/puppy1.jpg';
+    img.width="164";
+    img.height="82";
+    var foo = document.getElementById("myPicture");
+    foo.appendChild(img);
+    }
+}
+
