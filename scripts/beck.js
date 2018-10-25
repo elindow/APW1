730104303574ff3fa3d777decb9bf0571d270f1d
becksonstein <bs3153@columbia.edu>
2018-10-25 14:50:16 -0400

    add

--- scripts/beck.js
+++ scripts/beck.js
@@ -1,15 +1,40 @@
 var x;
 var y;
+var vector = new THREE.Vector3(mouse.x, mouse.y, 0.5);
+vector.unproject( camera );
+var dir = vector.sub( camera.position ).normalize();
+var distance = - camera.position.z / dir.z;
+var pos = camera.position.clone().add( dir.multiplyScalar( distance ) );
 
-var r= 100, g=100, b=100;
+var r= 200, g=100, b=100;
 
 function setup(){
  createCanvas(800,800,WEBGL);
  
- x = (mouseX)+ 1600;
- y = (mouseY)+1600;
+  x = (mouseX)-800;
+  y = (mouseY)-800;
 
   document.body.style.backgroundColor = "black";
+function onMouseMove( event ) {
+
+    if ( scope.enabled === false ) return;
+
+    event.preventDefault();
+
+    
+    
+    function onMouseMove(event) {
+
+    // Update the mouse variable
+    event.preventDefault();
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+    // Make the sphere follow the mouse
+    mouseMesh.position.set(event.clientX, event.clientY, 0);
+};
+
+  }
 
   
  // ellipse(x,y,50,50);
@@ -27,13 +52,13 @@ function draw(){
   pointLight(0,164,178,40,49,250);
   fill(r,g,b);
 
- translate(x, y, 0);
+ translate(x-400, y-400, 0);
  sphere(50);
 
  r = y+x;
  g = y -100;
  b = y -10;
- if(mouseX >255){
+ if(mouseX >500){
  r= y-(y-50);
  g = x-(y-50);
  
