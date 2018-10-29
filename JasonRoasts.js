df6f27ac5ba493671aad431d034503f1de1e32a8
jr3796 <31676104+jr3796@users.noreply.github.com>
2018-10-29 07:11:12 -0400

    I created Josh R is better than Kadhir for my paragraph

---
+++ JasonRoasts.js
@@ -0,0 +1,17 @@
+let num = 4 
+let randomR;
+let roastButton = document.body.getElementById("newbutton");
+
+// let Jasonsucks
+//  type="button" onclick= roast();
+
+console.log(randomR);
+
+roastButton.addEventListener(clicked, function (){
+	alert('Jason will not get into MIT') 
+	if (randomR == 0) {
+		alert('You are smart if clicked here!')
+	}else if(randomR == 1){
+		alert('Jason will not get into MIT') 
+	}
+}
