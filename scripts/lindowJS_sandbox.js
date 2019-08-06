1799d63acb24b8bb9451495437208b29a7a0a8c5
ELindow <elindow@verizon.net>
2019-08-06 14:25:34 -0400

    update JS sandbox page

--- scripts/lindowJS_sandbox.js
+++ scripts/lindowJS_sandbox.js
@@ -4,3 +4,13 @@ alert(`The rabbit says '${line}'`);   // note ‘${…} to insert var inside str
 };
 
 rabbit.speak("I'm alive."); 
+
+function speak(line) { 
+	console.log(`The ${this.type} rabbit says '${line}'`); 
+} 
+
+let whiteRabbit = {type: "white", speak}; 
+let hungryRabbit = {type: "hungry", speak};
+
+whiteRabbit.speak("Oh my ears and whiskers, " + "how late it's getting!"); 
+hungryRabbit.speak("I could use a carrot right now."); 
