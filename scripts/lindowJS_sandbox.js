f4f6e6f13abf5a53c674988264954715727b946d
ELindow <elindow@verizon.net>
2019-08-06 15:19:05 -0400

    update js to write html on click

--- scripts/lindowJS_sandbox.js
+++ scripts/lindowJS_sandbox.js
@@ -1,16 +1,23 @@
 let rabbit = {}; 					// instantiate empty object
 rabbit.speak = function(line) { 			// add new method
-alert(`The rabbit says '${line}'`);   // note ‘${…} to insert var inside string
+$("out2").html(`The rabbit says '${line}'`);   // note ‘${…} to insert var inside string
 };
 
-rabbit.speak("I'm alive."); 
-
+ 
 function speak(line) { 
-	console.log(`The ${this.type} rabbit says '${line}'`); 
+	$("out2").html(`The ${this.type} rabbit says '${line}'`); 
 } 
 
 let whiteRabbit = {type: "white", speak}; 
 let hungryRabbit = {type: "hungry", speak};
 
-whiteRabbit.speak("Oh my ears and whiskers, " + "how late it's getting!"); 
-hungryRabbit.speak("I could use a carrot right now."); 
+
+
+$("#LButton1").click(function() {  										// assign click() function to button using button id
+		//alert("Lindow button clicked");										// call alert just for testing purposes
+	rabbit.speak("I'm alive.");	
+	whiteRabbit.speak("Oh my ears and whiskers, " + "how late it's getting!"); 
+	hungryRabbit.speak("I could use a carrot right now."); 
+
+		$("#LButton1").html("I was clicked");								// change the button lable, or do whatever you want
+	});		
