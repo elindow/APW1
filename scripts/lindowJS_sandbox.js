18f268d151d172a07221a1b3d090df07e662d442
ELindow <elindow@verizon.net>
2019-08-06 13:07:47 -0400

    update JS sandbox page

--- scripts/lindowJS_sandbox.js
+++ scripts/lindowJS_sandbox.js
@@ -1,6 +1,6 @@
 let rabbit = {}; 					// instantiate empty object
 rabbit.speak = function(line) { 			// add new method
-console.log(`The rabbit says '${line}'`);   // note ‘${…} to insert var inside string
+alert(`The rabbit says '${line}'`);   // note ‘${…} to insert var inside string
 };
 
 rabbit.speak("I'm alive."); 
