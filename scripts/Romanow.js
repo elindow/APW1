ff7ae52cf025763035a0c7385ca1309b1fa2b517
Justin Romanow <justin@Justins-MacBook-Pro.local>
2019-09-18 11:09:06 -0400

    button and script

---
+++ scripts/Romanow.js
@@ -0,0 +1,3 @@
+ $("#LButton1").click(function() {  										// assign click() function to button using button id
+		alert("You can click");										// call alert just for testing purposes
+		$("#LButton1").html("click click");
