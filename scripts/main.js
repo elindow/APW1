5778df5c9ea288a3801951bdfcb4a3207a46d168
ELindow <elindow@trevor.org>
2020-11-09 11:33:50 -0500

    added quick info tab

--- scripts/main.js
+++ scripts/main.js
@@ -79,6 +79,19 @@ class Example {
 
 const MENU_BAR_SETTINGS = [
 	new Project(
+		"Quick Info",
+		"How to get out of Vim",
+		"ESC:wq",
+		[
+			new Area(
+				"",
+				[
+					//new Website("Version 1.0", "./Version 1.0/index.html"),
+				]
+			),
+		]
+	),
+	new Project(
 		"Older Versions",
 		"Previous Versions",
 		"This site is also managed and maintained every year by the senior Advanced Programmers. Whenever this site has a complete update, we make sure to archive the previous versions.",
