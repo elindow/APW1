0c701d4217536a204ad8275377ef3d112eb9bc8e
ELindow <elindow@verizon.net>
2021-07-31 19:53:37 -0400

    basic typeScript practice page

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+/** test of tScript*/
+var message = 'Time is Money';
+var heading = document.createElement('h1');
+heading.textContent = message;
+document.body.appendChild(heading);
+/*console.log(message);*/ 
---
+++ scripts/app.ts
@@ -0,0 +1,10 @@
+/** test of tScript*/
+
+let message: string = 'Time is Money';
+
+let heading = document.createElement('h1');
+heading.textContent = message;
+
+document.body.appendChild(heading);
+
+/*console.log(message);*/
