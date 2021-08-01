422d140122650d2466e5533789e284ac157ce2ac
Eric Lindow <elindow@trevor.org>
2021-07-31 21:21:42 -0400

    switched to teacher script file

---
+++ Teachers/Lindow/scripts/app.js
@@ -0,0 +1,6 @@
+/** test of tScript*/
+var message = 'Wisdom is Power';
+var heading = document.createElement('h1');
+heading.textContent = message;
+document.body.appendChild(heading);
+/*console.log(message);*/ 
---
+++ Teachers/Lindow/scripts/app.ts
@@ -0,0 +1,10 @@
+/** test of tScript*/
+
+let message: string = 'Wisdom is Power';
+
+let heading = document.createElement('h1');
+heading.textContent = message;
+
+document.body.appendChild(heading);
+
+/*console.log(message);*/
