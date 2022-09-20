c5dbe44d8f7699b5636f660c14aca8355b732524
Michael Pineda <mp3897@MacBook-Air.local>
2022-09-20 14:55:24 -0400

    added javascript to new index page

---
+++ Students/MichaelPineda/scripts/pineda_2022.js
@@ -0,0 +1,3 @@
+var img = document.createElement("img")
+img.src = "https://static.wikia.nocookie.net/for-all-mankind/images/a/a5/Sojourneraunch.png/revision/latest/scale-to-width-down/185?cb=20220624210606"
+document.body.appendChild(img)
