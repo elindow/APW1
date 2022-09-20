3d53d119816c32c82d29303e12d04a524d0474b4
Michael Pineda <mp3897@macbook-air.mynetworksettings.com>
2022-09-20 19:33:39 -0400

    added more js

--- Students/MichaelPineda/scripts/pineda_2022.js
+++ Students/MichaelPineda/scripts/pineda_2022.js
@@ -1,3 +1,8 @@
 var img = document.createElement("img")
 img.src = "https://static.wikia.nocookie.net/for-all-mankind/images/a/a5/Sojourneraunch.png/revision/latest/scale-to-width-down/185?cb=20220624210606"
 document.body.appendChild(img)
+
+var button = document.createElement("button")
+button.innerText = "testing"
+button.addEventListener("click", alert("Button Clicked"))
+document.body.appendChild(button)
